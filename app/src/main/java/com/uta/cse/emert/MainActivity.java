ad3545493047aa41886cdc49a14a8686c60f314f
Michelle <michelle.teague@mavs.uta.edu>
2018-10-20 10:59:02 -0500

    Initial commit

---
+++ app/src/main/java/com/uta/cse/emert/MainActivity.java
@@ -0,0 +1,13 @@
+package com.uta.cse.emert;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
